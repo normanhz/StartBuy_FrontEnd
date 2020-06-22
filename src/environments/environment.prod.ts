b3c4ff4bbbe548363680e5b3dbbdf79c525ad4c6
nhernan058 <normanhernandez011@gmail.com>
2020-06-22 16:17:55 -0600

    Ambientes de produccion actualizados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://localhost:44309/api/'
   // tslint:disable-next-line: comment-format
-  //API_URL: 'https://www.startbuyapp.com/api/'
+  //API_URL: 'https://localhost:44309/api/'
+  API_URL: 'https://www.startbuyapp.com/api/'
 };
